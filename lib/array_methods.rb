f93ae9b00bafcc2f0a7c60036bf3fe4851fa6a32
Doug Kintop <dkintop@hotmail.com>
2019-06-25 21:16:57 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,18 @@
 
 def find_element_index(array, value_to_find)
- 
+  
+   array.length.times do  |index|
+    array[index] = subject
+   if value_to_find == subject 
+    
+     index 
+  
+  else
+    nil
+
+end
+end
+end
   
   
 
